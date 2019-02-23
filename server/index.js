a73bac0bb06995d4af9e53d6eea145d2a24a5db1
Daniela Velásquez <angiedanielav@unicauca.edu.co>
2019-02-22 20:13:39 -0500

    user added to route

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ const express            = require('express');
 var app                  = express();
 var bodyParser           = require('body-parser');
 var models               = require('./models');
+var userRoutes            = require('./routes/user.route');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json({limit: 1048576}));
 
 //Sync with models
 models.sequelize.sync();
+
+//Assign routes
+app.use('/user', userRoutes);
 
 //Start the server
 var port = process.env.PORT || 1337;
